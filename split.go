d518a1f45f6b34539969d33d36b1c86769fffba5
Orlando Garcia <orlando.c@miro.com>
2024-01-28 01:27:55 +0100

    adds function to save one pdf

--- split.go
+++ split.go
@@ -2,6 +2,7 @@ package split
 
 import (
 	"log"
+	"os"
 
 	"go.k6.io/k6/js/modules"
 )
@@ -12,6 +13,16 @@ func init() {
 
 type PDF struct{}
 
-func (*PDF) SplitAndSave() {
+func (*PDF) SplitAndSave(pdfData []byte, filename string) {
 	log.Println("Split and save")
+
+	savePDFToFile(pdfData, filename)
+}
+
+func savePDFToFile(pdfData []byte, filename string) error {
+	err := os.WriteFile(filename, pdfData, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
 }
